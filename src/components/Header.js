a9eec2e221df36de728acf1373af9ab8ed530623
dansantander <dansantandr@gmail.com>
2020-12-03 10:15:20 -0600

    chore: add navbar dropdown

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,51 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Dropdown from 'react-bootstrap/Dropdown';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-  faShoppingCart, faUser, faSearch, faBell,
+  faBars,
 } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => (
-  <nav className="navbar navbar-expand-lg ">
-    <div className="container">
-      <div className="d-flex">
-        <h1>Shop</h1>
-      </div>
+  <div
+    id="nav-links"
+    className="nav-top pl-3 py-2 w-100 border-bottom d-flex justify-content-between"
+  >
+    <Dropdown>
+      <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+        <FontAwesomeIcon icon={faBars} />
+      </Dropdown.Toggle>
 
-      <div className="d-flex flex-column align-items-center">
-        <form className="form-inline my-2 my-lg-0">
-          <input className="form-control" type="search" placeholder="Search" aria-label="Search" />
-          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
-            <FontAwesomeIcon icon={faSearch} />
-          </button>
-        </form>
-        <ul className="navbar-nav">
-          <li className="mx-2 my-2">Category</li>
-          <li className="mx-2 my-2">History</li>
-          <li className="mx-2 my-2">Sell</li>
-          <li className="mx-2 my-2">Official Stores</li>
-          <li className="mx-2 my-2">Help</li>
-        </ul>
-      </div>
-
-      <div>
-        <ul className="navbar-nav">
-          <li className="mx-2">
-            <FontAwesomeIcon icon={faUser} />
-            <span className="ml-1">Daniel</span>
-          </li>
-          <li className="mx-2">
-            <span className="mr-1">Favorites</span>
-          </li>
-          <li className="mx-2">
-            <FontAwesomeIcon icon={faBell} />
-          </li>
-          <li className="mx-2">
-            <FontAwesomeIcon icon={faShoppingCart} />
-          </li>
-        </ul>
-      </div>
+      <Dropdown.Menu>
+        <Dropdown.Item as={Link} className="pl-2" to="/favorites">
+          Upload Products
+        </Dropdown.Item>
+        <Dropdown.Item as={Link} className="pl-2" to="/favorites">
+          Favorite Products
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+    <div className="mr-4 align-self-center">
+      Welcome
+      {' '}
+      <b>Daniel</b>
+      {' '}
+      !
     </div>
-  </nav>
+  </div>
 );
 
 export default Header;
