5827f8bb0535fbcdb4717dc75122fbc5b3e12149
dansantander <dansantandr@gmail.com>
2020-12-04 13:51:45 -0600

    feat: add favorite product to database

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,14 +5,10 @@ import axios from 'axios';
 /* eslint react/prop-types: 0 */
 /* eslint-disable no-console */
 const Product = ({ product, isFav, user }) => {
-  console.log('isFav', isFav);
-  console.log('user from props', user);
   function addFavorite() {
-    axios.post('http://localhost:3001/registrations', {
-      favProducts: {
-        product_id: product.id,
-        user_id: user.id,
-      },
+    axios.post('http://localhost:3001/favorites', {
+      product_id: product.id,
+      user_id: user.id,
     },
     { withCredentials: true }).then(response => {
       if (response.data.status === 'created') {
@@ -27,8 +23,26 @@ const Product = ({ product, isFav, user }) => {
     <div className="product col-12 col-md-3 my-4">
       <div className="card">
         {isFav
-          ? <i tabIndex={0} role="button" aria-label="Mute volume" className="fas fa-heart" onClick={() => addFavorite()} onKeyDown={() => addFavorite()} />
-          : <i className="far fa-heart" />}
+          ? (
+            <i
+              tabIndex={0}
+              role="button"
+              aria-label="Mute volume"
+              className="fas fa-heart"
+              onClick={() => console.log('working?')}
+              onKeyDown={() => console.log('working?')}
+            />
+          )
+          : (
+            <i
+              tabIndex={0}
+              role="button"
+              aria-label="Mute volume"
+              className="far fa-heart"
+              onClick={() => addFavorite()}
+              onKeyDown={() => addFavorite()}
+            />
+          )}
         <Link to={`/products/${product.id}`} id="link-product" data-testid="product_card">
           <img src={product.image} className="card-img-top" alt="..." />
           <div className="card-body">
